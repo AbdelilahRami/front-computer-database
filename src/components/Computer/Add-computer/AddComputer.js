6fa24a7b2a1b6378de129b961fb61253f55e99c1
RAMI <ramiabdelilah1@gmail.com>
2019-12-03 17:02:30 +0100

    begin-add

--- src/components/Computer/Add-computer/AddComputer.js
+++ src/components/Computer/Add-computer/AddComputer.js
@@ -1,4 +1,53 @@
-function AddComputer(){
+import React, { useState } from 'react';
 
-    
+function AddComputer() {
+
+
+
+    return (
+        <Form>
+
+      <FormGroup>
+        <Label for="exampleDatetime">Name</Label>
+        <Input
+          type="text"
+          name="computerName"
+          id="name"
+          placeholder="computer Name"
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="exampleDate">Introduced Date</Label>
+        <Input
+          type="date"
+          name="introduced"
+          id="introduced"
+          placeholder="Introduced date"
+        />
+      </FormGroup>
+      <FormGroup>
+      <Label for="exampleDate">Discontinued Date</Label>
+      <Input
+        type="date"
+        name="discontinued"
+        id="discontinued"
+        placeholder="discontinued date"
+      />
+    </FormGroup>
+
+      <FormGroup>
+        <Label for="exampleSelect">Company</Label>
+        <Input type="select" name="select" id="exampleSelect">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </Input>
+      </FormGroup>
+    </Form>
+    )
+
+
 }
+export default AddComputer;
--- src/components/Computer/Computers.js
+++ src/components/Computer/Computers.js
@@ -1,73 +1,84 @@
-import React , { useState } from 'react';
-import { useComputers,deleteComputers,searchName, countComputers } from '../../containers/computer/Computers.hook'
-import  Computer  from './Computer'
+import React, { useState } from 'react';
+import AddComputer from './Add-computer/AddComputer'
+import { useComputers, deleteComputers, searchName, countComputers } from '../../containers/computer/Computers.hook'
+import Computer from './Computer'
 import { Table } from 'reactstrap';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Input , Button , Label} from 'reactstrap';
+import { Input, Button, Label } from 'reactstrap';
 
-export function Computers(){
+export function Computers() {
 
-  const [statechampSearch,setchampSearch]=useState()
+  const [statechampSearch, setchampSearch] = useState()
+  const [addingMode, setAdding] = useState(false);
   console.log(useComputers())
+  console.log(addingMode);
+  const [computers] = useState(useComputers())
+  const count = useState(countComputers())
 
-  const [computers]=useState(useComputers())
-  const count=useState(countComputers())
+  let ids = []
 
-  let ids=[]  
-  
   function arrayRemove(arr, value) {
 
-    return arr.filter(function(ele){
-        return ele !== value;
+    return arr.filter(function (ele) {
+      return ele !== value;
     });
- 
- }
 
-  function checkFun(id){
-    if(!ids.includes(id)){
-        ids.push(id);
-    }else{
-       ids=arrayRemove(ids,id);
+  }
+
+  function checkFun(id) {
+    if (!ids.includes(id)) {
+      ids.push(id);
+    } else {
+      ids = arrayRemove(ids, id);
     }
     console.log(ids);
   }
 
-  function deleteFunction(){
+  function deleteFunction() {
     deleteComputers(ids);
   }
 
-  function showName(){
+  function showName() {
     searchName(statechampSearch);
   }
-  return(
+  return (
     <div>
-        <input style={{width:"300px",align:"center"}} type="text" placeholder="Veuillez saisir un nom de computer " onChange={event =>setchampSearch(event.target.value)} />
-        <Button onClick={() => showName()}>  Search </Button>
-        <br/>
-         <Label> Nombre d'ordinateurs : {count} </Label>
-         <Button>Add Computer</Button>
-        <br/>
-          <Table>
-            <thead>
-              <tr>
-                <th>#  <FontAwesomeIcon icon={faTrash} onClick={() => deleteFunction()}/> </th>
-                <th>Name</th>
-                <th>Introduced</th>
-                <th>Discontinued</th>
-                <th>Company</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {computers.map(computer =>
-                    <Computer computer={computer}
-                              key={computer.id}
-                              checkFun={checkFun}
-                    />
-              )}         
-            </tbody>
-        </Table>
-      </div>
+    <Button className="btn btn-secondary float-right" onClick={() => setAdding(!addingMode)}>Add Computer</Button>
+      <input style={{ width: "300px", align: "center" }} type="text" placeholder="Veuillez saisir un nom de computer " onChange={event => setchampSearch(event.target.value)} />
+      <Button onClick={() => showName()}>  Search </Button>
+      <br />
+      <Label> Nombre d'ordinateurs : {count} </Label>
+      <br />{
+
+        !addingMode ?
+          <>
+            <Table>
+              <thead>
+                <tr>
+                  <th>#  <FontAwesomeIcon icon={faTrash} onClick={() => deleteFunction()} /> </th>
+                  <th>Name</th>
+                  <th>Introduced</th>
+                  <th>Discontinued</th>
+                  <th>Company</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {computers.map(computer =>
+                  <Computer computer={computer}
+                    key={computer.id}
+                    checkFun={checkFun}
+                  />
+                )}
+              </tbody>
+            </Table>
+          </>
+          :
+          <>
+            <AddComputer />
+          </>
+      }
+    </div>
   )
 }
