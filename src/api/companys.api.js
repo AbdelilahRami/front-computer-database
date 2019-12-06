ba0546cfb4a848bdc758b3ee00cde75ba2f40622
Pierre*git <xx-tower@hotmail.fr>
2019-12-06 10:43:39 +0100

    Add request company

--- src/api/companys.api.js
+++ src/api/companys.api.js
@@ -2,25 +2,26 @@ import Axios from 'axios'
 export const adresse = "http://localhost:8080/computer-database-app/companys"
 
 export async function getCompanys(){
-    await Axios.get(adresse)
-    .then(response=>{return response})
-    .catch(error=> console.log(error))
+    return await Axios.get(adresse)
+    .then(response=>{console.log(response)
+        return response})
+    .catch(error=> console.log(error));
 }
 
 export async function addCompany(company){
-    await Axios.post(adresse,company)
+    return await Axios.post(adresse,company)
     .then(response => {return response})
     .catch(error=>console.error(error))
 }
 
 export async function deleteCompany(company){
-    await Axios.delete(adresse,company)
+    return await Axios.delete(adresse,company)
     .then(response => {return response})
     .catch(error=>console.error(error))
 }
 
-export async function updateCompany(company){
-    await Axios.put(adresse,company)
+export async function updateCompani(company){
+    return await Axios.put(adresse,company)
     .then(response => {return response})
     .catch(error=>console.error(error))
 }
--- src/components/Company/EditCompany.js
+++ src/components/Company/EditCompany.js
@@ -1,25 +1,26 @@
 import React, { useState, useEffect } from 'react';
-function EditCompanyForm(props) {
-    const [company,setCompany] = useState(props.currentCompany)
-            useEffect(
-                () => {
-                    setCompany(props.currentCompany)
-                },
-                [props]
-            )
-          return(
-                <form 
-                    onSubmit={event => {
-                        event.preventDefault()
-                        if (!company.name) return
-                        props.updateCompany(company.id,company)
-                    }}
-                >
-                    <label>Name</label>
-                    <input type="text" name="name" value={company.name} onChange={event=>setCompany({...company,name:event.target.value})} />
-                    <button>Update Company</button>
 
-                </form>
-          )
+function EditCompanyForm(props) {
+
+    const [company, setCompany] = useState(props.currentCompany)
+    useEffect(
+        () => {setCompany(props.currentCompany)},
+        [props]
+    )
+
+    return (
+        <form
+            onSubmit={event => {
+                event.preventDefault()
+                if (!company.name) return
+                props.updateCompany(company.id, company)
+            }}
+        >
+            <label>Name</label>
+            <input type="text" name="name" value={company.name} onChange={event => setCompany({ ...company, name: event.target.value })} />
+            <button>Update Company</button>
+
+        </form>
+    )
 }
 export default EditCompanyForm;
