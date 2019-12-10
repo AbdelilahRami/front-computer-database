e0ca9f7b0b11da4d81f8bfae747779dd738fae81
Pierre*git <xx-tower@hotmail.fr>
2019-12-10 17:45:04 +0100

    fix error

--- src/components/Computer/Add-computer/AddComputer.js
+++ src/components/Computer/Add-computer/AddComputer.js
@@ -9,16 +9,16 @@ import { Redirect } from 'react-router-dom';
 
 export default function AddComputer() {
 
-  const [ajout,setAjout] = useState(false)
+  const [ajout, setAjout] = useState(false)
   const [companies, setCompanies] = useState([]);
   const initialsForm = { id: null, name: '', introduced: '', discontinued: '', companyDTO: { id: null, name: '' } };
   const [computer, setComputer] = useState(initialsForm);
   const { handleSubmit, errors } = useForm(
     submit,
-    validate,computer
+    validate, computer
   );
 
-  function cancelForm(){
+  function cancelForm() {
 
     setAjout(true)
   }
@@ -30,8 +30,8 @@ export default function AddComputer() {
     setComputer({ ...computer, companyDTO: companyDTOX })
   }
 
-  function submit(){
-    addComputer(computer).then(()=>setAjout(true))
+  function submit() {
+    addComputer(computer).then(() => setAjout(true))
   }
 
 
@@ -40,76 +40,77 @@ export default function AddComputer() {
     setComputer({ ...computer, [name]: value })
   }
 
-  useEffect(() =>{
+  useEffect(() => {
     getCompanies().then(
       response => {
         setCompanies(response.data || [])
       }
     )
   }
-  , [])
+    , [])
 
   return (
     <>
-    {ajout  ?  <Redirect to='/computers' />
-      
-    :
-    <Form className="form"
-      onSubmit={handleSubmit}
+      {ajout ? <Redirect to='/computers' />
 
-    >
-    <div style={{textAlign:'center'}}>
-        <h2 style={{color:'#6c757d'}}>Adding Computer Form</h2>
-    </div>
-      <FormGroup>
-        <Label for="exampleDatetime">Name</Label>
-        <Input
-          type="text"
-          name="name"
-          value={computer.name}
-          onChange={handleChange}
-          id="name"
-          placeholder="computer Name"
-        />
-        {errors.name && <p className="Stylish">{errors.name}</p>}
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleDate">Introduced Date</Label>
-        <Input
-          type="date"
-          name="introduced"
-          id="introduced"
-          value={computer.introduced}
-          onChange={handleChange}
-          placeholder="Introduced date"
-        />
-        {errors.introduced && <p className="Stylish">{errors.introduced}</p>}
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleDate">Discontinued Date</Label>
-        <Input
-          type="date"
-          value={computer.discontinued}
-          name="discontinued"
-          onChange={handleChange}
-          id="discontinued"
-          placeholder="discontinued date"
-        />
-        {errors.discontinued && <p className="Stylish">{errors.discontinued}</p>}
+        :
+        <Form className="form"
+          onSubmit={handleSubmit}
 
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleSelect">Company</Label>
-        <Input type="select" onChange={getCompanyDTO} name="select" id="exampleSelect">
-          <option key="0" value=""></option>
-          {companies.map(company => <option value={company.name} key={company.id} >{company.name}</option>)}
-        </Input>
-      </FormGroup>
-      <div style={{textAlign:'center'}}>
-            <button style={{ color: 'white', backgroundColor: '#17a2b8', borderColor:'#17a2b8'}}>Add Computer</button>&nbsp;&nbsp;&nbsp;
-            <button type="button" onClick={cancelForm} className="button muted-button">Cancel</button>  
+        >
+          <div style={{ textAlign: 'center' }}>
+            <h2 style={{ color: '#6c757d' }}>Adding Computer Form</h2>
+          </div>
+          <FormGroup>
+            <Label for="exampleDatetime">Name</Label>
+            <Input
+              type="text"
+              name="name"
+              value={computer.name}
+              onChange={handleChange}
+              id="name"
+              placeholder="computer Name"
+            />
+            {errors.name && <p className="Stylish">{errors.name}</p>}
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleDate">Introduced Date</Label>
+            <Input
+              type="date"
+              name="introduced"
+              id="introduced"
+              value={computer.introduced}
+              onChange={handleChange}
+              placeholder="Introduced date"
+            />
+            {errors.introduced && <p className="Stylish">{errors.introduced}</p>}
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleDate">Discontinued Date</Label>
+            <Input
+              type="date"
+              value={computer.discontinued}
+              name="discontinued"
+              onChange={handleChange}
+              id="discontinued"
+              placeholder="discontinued date"
+            />
+            {errors.discontinued && <p className="Stylish">{errors.discontinued}</p>}
 
-      <p></p>
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleSelect">Company</Label>
+            <Input type="select" onChange={getCompanyDTO} name="select" id="exampleSelect">
+              <option key="0" value=""></option>
+              {companies.map(company => <option value={company.name} key={company.id} >{company.name}</option>)}
+            </Input>
+          </FormGroup>
+          <div style={{ textAlign: 'center' }}>
+            <button style={{ color: 'white', backgroundColor: '#17a2b8', borderColor: '#17a2b8' }}>Add Computer</button>&nbsp;&nbsp;&nbsp;
+            <button type="button" onClick={cancelForm} className="button muted-button">Cancel</button>
+
+            <p></p>
+          </div>
     </Form>}
     </>
   )
