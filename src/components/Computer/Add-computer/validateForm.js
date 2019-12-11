1b83faa4f3fbdd97ae5bdf4370211a21aac761db
Pierre*git <xx-tower@hotmail.fr>
2019-12-11 15:17:37 +0100

    Fix message erreur

--- src/components/Computer/Add-computer/validateForm.js
+++ src/components/Computer/Add-computer/validateForm.js
@@ -1,19 +1,19 @@
 export default function validate(computer) {
     let errors = {};
     if (!computer.name) {
-        errors.name = "computer name is not empty";
+        errors.name = "computer name is empty";
     }
 
     if (Date.parse(computer.introduced) > Date.parse(computer.discontinued)) {
-        errors.introduced = "introduced date  is before discontinued date";
+        errors.introduced = "introduced date is before discontinued date";
     }
 
     if (Date.parse(computer.introduced) > new Date()) {
-        errors.introduced = "date cannot exced today date";
+        errors.introduced = "date cannot exceed the current date";
     }
 
     if (Date.parse(computer.discontinued) > new Date()) {
-        errors.discontinued = "date cannot exced today date";
+        errors.discontinued = "date cannot exceed the current date";
     }
 
     if (Date.parse(computer.introduced) < new Date('1970-01-01')) {
