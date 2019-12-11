3e9b399334d1e4e64d484bf25868ff76919f0fd0
RAMI <ramiabdelilah1@gmail.com>
2019-12-11 17:31:51 +0100

    Fix footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -11,7 +11,8 @@ export default function Footer({ recupererLimite, maxPage, recupererActualPage, 
     const [statedroite, setdroite] = useState(true)
 
     function iterateButton() {
-        if(actPage>maxPage){setActPage(1)}
+        if(actPage>maxPage){setActPage(1) 
+            setgauche(false)}
         if (actPage === 1) {
             if (maxPage === 3) {
                 return (
